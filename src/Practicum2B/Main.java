8a07cba099837e6e592d31a27ef57ad5076fdfdb
Ryan Reddy <90551370+klugook@users.noreply.github.com>
2022-02-10 19:49:22 +0100

    finished prakticum 2B

--- src/Practicum2B/Main.java
+++ src/Practicum2B/Main.java
@@ -1,7 +1,25 @@
 package Practicum2B;
 
 public class Main {
-    public static void main(String[] arg) {
+  public static void main(String[] args) {
+    Voetbalclub ajx = new Voetbalclub("Ajax      ", 0, 0, 0);
+    Voetbalclub feij = new Voetbalclub("Feijenoord", 0, 0, 0);
 
-    }
+    feij.verwerkResultaat('w');
+    feij.verwerkResultaat('w');
+    feij.verwerkResultaat('w');
+    feij.verwerkResultaat('g');
+
+//    ajx.verwerkResultaat('w');
+//    ajx.verwerkResultaat('w');
+//    ajx.verwerkResultaat('w');
+//    ajx.verwerkResultaat('w');
+
+    System.out.println("Feijenoord punten: " + feij.aantalPunten());
+    System.out.println("Ajax gespeeld: " + ajx.aantalGespeeld());
+    System.out.println();
+
+    System.out.println(ajx);
+    System.out.println(feij);
+  }
 }
